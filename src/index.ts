fff3e054aaa20028104c9dccd30ba3fc8179d464
Joren Six <joren.six@ugent.be>
2024-06-28 16:18:57 +0200

    export

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-//import ATFTokenizer from "./components/ATFTokenizer.vue";
-import ATFSignSelector from "./lib/ATFSignSelector";
-
-export default{ 
-    components: {
-        
-        ATFSignSelector
-    },
-    
-    ATFSignSelector
-};
 
 export * from "./types";
+export * from '@/lib/ATFSignSelector'
