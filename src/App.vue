60a8e0fa0e42448f0a6b7854ce1b62f0f080f51d
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-07-08 20:33:02 +0200

    added atf picklist

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import ATFTokenizer from '@/components/ATFTokenizer.vue';
 //import {ATFItemSign} from './types/CuniformTypes';
 import ATFSignSelector from '@/lib/ATFSignSelector.ts';
-import {ref } from 'vue';
+import {computed, ref } from 'vue';
 
 const activeATFSigns = ref<Array<ATFSignSelector>>([]);
 
@@ -51,21 +51,26 @@ const itemToSelector = (item) => {
 
 const onMouseEnter = (item) => {
     hoveredATFSigns.value.push(itemToSelector(item));
-    console.log('APP: mouse enter', hoveredATFSigns.value.length);
+    console.log('APP: mouse enter', item, hoveredATFSigns.value.length);
 }
 
 const onMouseLeave = (item) => {
     hoveredATFSigns.value.splice(0, hoveredATFSigns.value.length);
+    console.log('APP: mouse leave', item, hoveredATFSigns.value.length);
 }
 
 const clickLevel = ref('sign');
 
-const atf = ref('');
+const atf = computed(() => {
+    return tablets.value.find((t) => t.id === tabletId.value)?.transliteration_atf
+})
+const tabletId = ref('null')
+const tablets = ref([])
 
-fetch('/data/O_219.atf')
+fetch('/data/tablets.json')
     .then(response => response.text())
     //.then(data => atf.value = data)
-    .then(data => atf.value = "@tablet\r\n@obverse\r\n1. 1(u)# 1(asz)# IKU A.SZA3 {gisz}KIRI6\r\n2. DA A.SZA3 la-la-ti-ia\r\n3. u3 DA na-ra-am-tum \r\n4. SAG.<BI>.1(disz).KAM hu-nu-nu-um \r\n5. [\u2026] SAG.<BI>.2(disz).KAM nu-ur2-be-el-ti-ni \r\n6.  SZAM2.TIL.LA.BI.SZE3 \r\n7. 1(u) GIN2 KU3.BABBAR \r\n8. IN.NA.LA2 \r\n9. KI ur-{gisz}GIGIR \r\n10. la-la-ti-ia \r\n11. IN.SZI.SA10 \r\n12. U4#.KUR2#.SZE3 INIM NU.GA2.GA2.A\r\n@reverse\r\n1. MU {d}LUGAL-MAR2.DA \r\n2. u3 su-mu-nu-um-hi-im \r\n3. IN.PA3.DE3.ESZ \r\n4. IGI sza?-bi-gi4-ri-is DUMU mil-{d}EN.ZU \r\n5. IGI na-bi-{d}EN.ZU DUMU be-la-nu-um \r\n6. IGI ku3-{d}NIN.SZUBUR DUMU DINGIR-ba-ni \r\n7. IGI {d}EN.ZU-li-di-isz DUMU DINGIR-ba-ni \r\n8. IGI nu-ur2-be-el-ti-ni SZESZ.A.NI \r\n9. IGI ib-ni-ir3-ra DUMU i3-li2-ba-asz-;ur?-la-ku \r\n10. IGI a-hu-szi-na DUMU {d}EN.ZU/LIL2-GAL?.ZU? \r\n11. IGI ar-wi-um DUB.SAR \r\n12. IGI na-bi-i3-li2-szu DUMU i3-li2-ba-asz;-ur?-la-ku \r\n13. ITI# APIN#.DU8?.A\r\n@top\r\n1. [MU {gisz}]GU#.ZA BARA2? {d}NIN.[...];-RE \r\n2. MU#.NA.DIM2\r\n@seal 1\r\n1. ur-{gisz}GIGIR \r\n2. DUMU x dul/ku4/tu");
+    .then(data => tablets.value = JSON.parse(data));
 
 
 const namedEntities = ref([]);
@@ -91,9 +96,12 @@ const namedEntities = ref([]);
                         <option value="line">Line</option>
                         <option value="part">Part</option>
                     </select>
+                    <select v-model="tabletId">
+                        <option v-for="tablet of tablets" :value="tablet.id" :key="'tb-option' + tablet.id">{{ tablet.id }}</option>
+                    </select>
                 </div>
                 
-                <ATFTokenizer 
+                <ATFTokenizer v-if="atf"
 
                     :atf="atf"
                    
